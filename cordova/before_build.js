9e2471d5c938028f999e0e35f47d4176531d9ec7
Gregivy <gpoi@mail.ru>
2016-10-18 22:48:09 +0300

    fixes

--- cordova/before_build.js
+++ cordova/before_build.js
@@ -1,14 +1,14 @@
 module.exports = function(ctx) {
     // Убедитесь что платформа android является частью построения 
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path');
+
     console.log("entering the hook script");
+
     if (ctx.opts.platforms.indexOf('android') > -1) {
-        var fs = ctx.requireCordovaModule('fs'),
-            path = ctx.requireCordovaModule('path');
-
         var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
-        
-        console.log("before_build----",path.join(platformRoot,"google-services.json"),fs.readFileSync("google-services.json"));
-
-        fs.writeFileSync(path.join(platformRoot,"google-services.json"), fs.readFileSync("google-services.json"));
+        var gsjson = fs.readFileSync(path.join(platformRoot,"app/www/google-services.json"));
+        console.log("before_build----",path.join(platformRoot,"google-services.json"),gsjson);
+        fs.writeFileSync(path.join(platformRoot,"google-services.json"), gsjson);
     }
 };
