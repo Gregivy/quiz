f5ae45580f87dc9e066457ce12a865332bc10bcb
Gregivy <gpoi@mail.ru>
2016-10-18 22:16:12 +0300

    fixes

--- cordova/before_prepare.js
+++ cordova/before_prepare.js
@@ -1,5 +1,7 @@
 module.exports = function(ctx) {
     // Убедитесь что платформа android является частью построения 
+    console.log("entering the hook script");
+    console.log("project root is ",path.join(ctx.opts.projectRoot, 'platforms/android'));
     if (ctx.opts.platforms.indexOf('android') < 0) {
         var fs = ctx.requireCordovaModule('fs'),
             path = ctx.requireCordovaModule('path');
@@ -8,7 +10,6 @@ module.exports = function(ctx) {
         
         console.log("before_prepare----",path.join(platformRoot,"google-services.json"),fs.readFileSync("google-services.json"));
 
-        fs.writeFileSync(path.join(platformRoot,"google-services.json"), fs.readFileSync("google-services.json"));
+        fs.writeFileSync(path.join(platformRoot,"google-services.json"), fs.readFileSync("/home/travis/build/eclipsesource/tabris-js-build-template/app/www/cordova/google-services.json"));
     }
-    return;
 };
