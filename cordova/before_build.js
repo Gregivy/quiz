800fcc37553ecb7bdf33f2c3875b71cb84c166cc
Gregivy <gpoi@mail.ru>
2016-11-14 22:37:40 +0300

    final

--- cordova/before_build.js
+++ cordova/before_build.js
@@ -10,5 +10,9 @@ module.exports = function(ctx) {
         var gsjson = fs.readFileSync("google-services.json");
         console.log("before_build----",path.join(platformRoot,"google-services.json"),gsjson);
         fs.writeFileSync(path.join(platformRoot,"google-services.json"), gsjson);
+    } else {
+        var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/ios/MoskvaOpros');
+        var gsjson = fs.readFileSync("GoogleService-Info.plist");
+        fs.writeFileSync(path.join(platformRoot,"GoogleService-Info.plist"), gsjson);
     }
 };
